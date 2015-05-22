9ada430bf6ab392e2fc4533ac156792ca1c0c8b2
Tom Willmot <tom@humanmade.co.uk>
2015-05-22 16:37:27 +0100

    make sure  is actually available

--- uninstall.php
+++ uninstall.php
@@ -5,5 +5,7 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	die;
 }
 
+global $wpdb;
+
 // delete all options and transients that contain mimi
 $wpdb->query( "DELETE FROM $wpdb->options WHERE option_name LIKE '%mimi-%'" );
