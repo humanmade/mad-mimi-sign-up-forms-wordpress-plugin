05f5a9108c3003d97f72d9032b212e2ea60bf5d6
Tom Willmot <tom@humanmade.co.uk>
2015-05-22 15:29:14 +0100

    properly remove transients and options on uninstall

--- uninstall.php
+++ uninstall.php
@@ -5,4 +5,5 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	die;
 }
 
-// silence is golden.
+// delete all options and transients that contain mimi
+$wpdb->query( "DELETE FROM $wpdb->options WHERE option_name LIKE '%mimi-%'" );
